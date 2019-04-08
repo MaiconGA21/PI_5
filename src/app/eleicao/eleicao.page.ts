881d5aa906398501fa1db27cdfba7786741f58f2
edilsonss123 <edilson016@outlook.com>
2019-04-08 20:32:46 -0300

    criacoa da view votacao

--- src/app/eleicao/eleicao.page.ts
+++ src/app/eleicao/eleicao.page.ts
@@ -1,6 +1,8 @@
 
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EleicaoService } from '../eleicao.service';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-eleicao',
@@ -9,11 +11,50 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EleicaoPage implements OnInit {
 
-  constructor(private route: ActivatedRoute ) { }
-  id:any;
+  constructor(private route: ActivatedRoute, public eleicaoService: EleicaoService, public db: AngularFireDatabase) { }
+  eleicao: any;
+  nome: string;
+  id: number;
+  candidatos: any;
+  tipo: string;
+  data: any;
+  votosTotal: number;
+  idEleicao: number;
+  caminho: string;
+votacao:number;
   ngOnInit() {
-    this.id = this.route.snapshot.paramMap.get('id');
-    alert(this.id);
+    this.nome = this.eleicaoService.getNome();
+    this.id = this.eleicaoService.getId();
+    this.candidatos = this.eleicaoService.getCandidatos();
+    this.tipo = this.eleicaoService.getTipo();
+    this.data = this.eleicaoService.getData();
+    this.votosTotal = this.eleicaoService.getVotosTotal();
+    this.idEleicao = this.eleicaoService.getIdEleicao();
+    this.votacao = 0;
+    console.log(this.candidatos);
   }
 
+  gravarVoto(NumCandidato,) {
+    if (this.tipo == 'PR') {
+      this.caminho = '/eleicao/privado/';
+    } else {
+      this.caminho = '/eleicao/publica/';
+    }
+    this.caminho +=this.idEleicao;
+
+    this.db.object('/eleicao/privada/votacao1/candidatos/v')
+   
+  }
+/*cls
+  getId(candidato) {
+    let id = -1;
+    for (let index = 0; index < this.candidatos.length; index++) {
+      if (this.candidatos[index].NumCandidato === candidato.NumCandidato) {
+        id = index;
+        break;
+      }
+    }
+    return id;
+  }
+*/
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { AngularFireDatabase } from 'angularfire2/database';
-
-
-
+import { EleicaoService } from '../eleicao.service';
 
 @Component({
   selector: 'app-tab2',
@@ -14,13 +12,33 @@ import { AngularFireDatabase } from 'angularfire2/database';
 
 
 export class Tab2Page {
-   lista:any;
+  listaPr: any;
+  listaPu: any;
+ 
 
-  constructor(public navCtrl: NavController, public db: AngularFireDatabase) {
-    this.lista = this.db.list('/eleicao').valueChanges();
+  constructor(public navCtrl: NavController, public db: AngularFireDatabase,  public eleicaoService:EleicaoService) {
+    this.listaPr = this.db.list('/eleicao/privada').valueChanges();
+    this.listaPu = this.db.list('/eleicao/publica').valueChanges();
+    
+  }
+  
+  abrirEleicao(eleicao,idEleicao) {
+    this.eleicaoService.setCandidatos(eleicao.candidatos);
+    
+    this.eleicaoService.setNome(eleicao.nome);
+    this.eleicaoService.setTipo(eleicao.tipo);
+    this.eleicaoService.setData(eleicao.data);
+    this.eleicaoService.setId(eleicao.eleicao);
+    this.eleicaoService.setVotosTotal(eleicao.votosTotal);
+    this.eleicaoService.setIdEleicao(idEleicao);
+    this.navCtrl.navigateForward('eleicao');
+
   }
 
-  abrirEleicao(id){
-    this.navCtrl.navigateForward('eleicao/'+id);
+  sliderConfig = {
+    slidesPerView: 1.6,
+    spaceBetween: 10,
+    centeredSlides: true
   }
 }
+
